be97b178dcc861f3f84d9bb37374673ed18fee12
batuhanckmkc <>
2025-10-12 12:47:48 +0300

    Remove unused scriptable objects

--- Assets/Scripts/Grid/CellData.cs
+++ Assets/Scripts/Grid/CellData.cs
@@ -8,18 +8,11 @@ namespace BusJamDemo.Grid
         public Cell Cell { get; private set; }
         public CellItem CellItem { get; private set; }
         public CellPosition CellPosition { get; private set; }
-        public CellData_SO SourceSO { get; private set; }
         public CellData(CellPosition cellPosition)
         {
             CellPosition = cellPosition;
         }
         
-        public void SetSourceSO(CellData_SO sourceSo)
-        {
-            SourceSO = sourceSo;
-        }
-
-
         public void FillCell(Cell grid)
         {
             Cell = grid;
